6174002337cc52f238c8848dc9b08a5e3774e639
Kenjie12117 <21-79778@g.batstate-u.edu.ph>
2025-07-14 16:06:45 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const factBox = document.getElementById('fact-box');
+const getFactBtn = document.getElementById('get-fact-btn');
+const errorMessage = document.getElementById('error-message');
+
+getFactBtn.addEventListener('click', () => {
+  fetch('https://catfact.ninja/fact')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('API request failed');
+      }
+      return response.json();
+    })
+    .then(data => {
+      factBox.textContent = data.fact;
+      errorMessage.textContent = '';
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      errorMessage.textContent = 'Could not load a cat fact.Try again!!!';
+    });
+});
